0abd3551552e6cbe6148e9acbd5853425c11f3e1
dududisk <nartuhh@gmail.com>
2022-07-26 23:19:21 +0000

    atualizacao do html

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+from datetime import datetime
+
+posts = [
+    {
+        "title": "O meu Primeiro Post", 
+        "body": "Aqui é o texto do Post", 
+        "author": "Feulo", 
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "O meu Segundo Post", 
+        "body": "Aqui é o texto do Post", 
+        "author": "Danilo", 
+        "created": datetime(2022,7,26)
+    },
+]
+
 
 app = Flask("hello")
 
 @app.route("/")
-def hello():
-    return "Hello Word"
-
-@app.route("/meucontato")
-def meucontato():
-    return render_template('index.html')
+def index():
+    return render_template("index.html", posts=posts)
