ea7b3ec20fd21bb9ac64d7c0004f299d4f2e48fc
dududisk <nartuhh@gmail.com>
2022-07-28 00:30:45 +0000

    inclusao do bd no blog

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
-
-posts = [
-    {
-        "title": "O meu Primeiro Post", 
-        "body": "Aqui é o texto do Post", 
-        "author": "Feulo", 
-        "created": datetime(2022,7,25)
-    },
-    {
-        "title": "O meu Segundo Post", 
-        "body": "Aqui é o texto do Post", 
-        "author": "Dudu", 
-        "created": datetime(2022,7,26)
-    },
-]
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("hello")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    _tablename_ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(500))
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeingKey('users.id'))
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(25), nullable=False, unique=True, index=True)
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author')
+
+db.create_all()
 
 @app.route("/")
 def index():
+    posts = Post.query.all()
     return render_template("index.html", posts=posts)
 
+@app.route("/populate")
+def populate():
+    post1 = Post(title="Post 1", body="Texto do Post", author="Feulo")
+    post2 = Post(title="Post 2", body="Texto do Post 2", author="Feulo")
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
+    return redirect(url_for('index'))
